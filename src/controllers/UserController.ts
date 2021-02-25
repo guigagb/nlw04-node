4a989b624df827fb2c8af2e4923a5e80705cf6ea
Guilherme <bsbtrindade@gmail.com>
2021-02-24 21:34:47 -0300

    fix: corrigir status do create user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,24 +3,24 @@ import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
-  async create(request: Request, response: Response) {
-    const { name, email } = request.body;
+	async create(request: Request, response: Response) {
+		const { name, email } = request.body;
 
-    const usersRepository = getCustomRepository(UsersRepository);
+		const usersRepository = getCustomRepository(UsersRepository);
 
-    const userAlreadyExists = await usersRepository.findOne({ email });
+		const userAlreadyExists = await usersRepository.findOne({ email });
 
-    if (userAlreadyExists)
-      return response.status(400).json({
-        error: "User already exists",
-      });
+		if (userAlreadyExists)
+			return response.status(400).json({
+				error: "User already exists",
+			});
 
-    const user = usersRepository.create({ name, email });
+		const user = usersRepository.create({ name, email });
 
-    await usersRepository.save(user);
+		await usersRepository.save(user);
 
-    return response.json(user);
-  }
+		return response.status(201).json(user);
+	}
 }
 
 export { UserController };
